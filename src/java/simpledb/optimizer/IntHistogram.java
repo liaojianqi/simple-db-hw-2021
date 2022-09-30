8ed4b802c4f85e38dc81523c0ed0b8c22750c67c
LoinLiao <loinliao@163.com>
2022-09-30 17:13:54 +0800

    [lab3] complete execise 1

--- src/java/simpledb/optimizer/IntHistogram.java
+++ src/java/simpledb/optimizer/IntHistogram.java
@@ -1,10 +1,19 @@
 package simpledb.optimizer;
 
+import simpledb.common.DbException;
 import simpledb.execution.Predicate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /** A class to represent a fixed-width histogram over a single integer-based field.
  */
 public class IntHistogram {
+    List<Integer> bucketList;
+    int width;
+    int min;
+    int max;
 
     /**
      * Create a new IntHistogram.
@@ -24,6 +33,23 @@ public class IntHistogram {
      */
     public IntHistogram(int buckets, int min, int max) {
     	// some code goes here
+        this.min = min;
+        this.max = max;
+        int maxBucket = max - min + 1;
+        if (buckets > maxBucket) {
+            buckets = maxBucket;
+        }
+
+        if ((max - min + 1) % buckets != 0) {
+            max += buckets - (max - min + 1) % buckets;
+        }
+
+        width = (max - min + 1) / buckets;
+
+        bucketList = new ArrayList<>();
+        for (int i = 0; i < buckets; i++) {
+            bucketList.add(0);
+        }
     }
 
     /**
@@ -31,7 +57,9 @@ public class IntHistogram {
      * @param v Value to add to the histogram
      */
     public void addValue(int v) {
-    	// some code goes here
+        // some code goes here
+        int index = (v - min) / width;
+        this.bucketList.set(index, this.bucketList.get(index) + 1);
     }
 
     /**
@@ -45,11 +73,101 @@ public class IntHistogram {
      * @return Predicted selectivity of this particular operator and value
      */
     public double estimateSelectivity(Predicate.Op op, int v) {
-
-    	// some code goes here
-        return -1.0;
+    	  // some code goes here
+        if (op == Predicate.Op.EQUALS) {
+            return estimateSelectivity_Equals(v);
+        } else if (op == Predicate.Op.NOT_EQUALS) {
+            return 1 - estimateSelectivity_Equals(v);
+        } else if (op == Predicate.Op.GREATER_THAN) {
+            return estimateSelectivity_GreatThan(v, false);
+        } else if (op == Predicate.Op.GREATER_THAN_OR_EQ) {
+            return estimateSelectivity_GreatThan(v, true);
+        } else if (op == Predicate.Op.LESS_THAN) {
+            return estimateSelectivity_LessThan(v, false);
+        } else if (op == Predicate.Op.LESS_THAN_OR_EQ) {
+            return estimateSelectivity_LessThan(v, true);
+        } else {
+            try {
+                throw new DbException(String.format("invalid op %s", op));
+            } catch (DbException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
-    
+
+    private double estimateSelectivity_Equals(int v) {
+        int index = (v - min) / width;
+        int h = this.bucketList.get(index);
+
+        return ((double)h / width) / nTups();
+    }
+
+    private double estimateSelectivity_GreatThan(int v, boolean withEqual) {
+        if (v <= min) {
+            return 1;
+        }
+        if (withEqual) {
+            if (v > max) {
+                return 0;
+            }
+        } else {
+            if (v >= max) {
+                return 0;
+            }
+        }
+
+        int index = (v - min) / width;
+        int h = this.bucketList.get(index);
+
+        double b_f = (double) h / nTups();
+        int b_right_len = width -  (v - min) % width + 1;
+        if (withEqual) b_right_len++;
+        double b_part = b_right_len / (double) width;
+
+        return b_f * b_part + getSelectivity(index + 1, this.bucketList.size() - 1);
+    }
+
+    private double estimateSelectivity_LessThan(int v, boolean withEqual) {
+        if (v >= max) {
+            return 1;
+        }
+        if (withEqual) {
+            if (v < min) {
+                return 0;
+            }
+        } else {
+            if (v <= min) {
+                return 0;
+            }
+        }
+
+        int index = (v - min) / width;
+        int h = this.bucketList.get(index);
+
+        double b_f = (double) h / nTups();
+        int b_left_len = (v - min) % width;
+        if (withEqual) b_left_len++;
+
+        double b_part = b_left_len / (double) width;
+
+        return b_f * b_part + getSelectivity(0, index - 1);
+    }
+
+    // [s, e]
+    private double getSelectivity(int s, int e) {
+        s = Math.max(s, 0);
+        e = Math.min(e, this.bucketList.size() - 1);
+        int sum = 0;
+        for (int i = s; i <= e; i++) {
+            sum += this.bucketList.get(i);
+        }
+        return sum / (double) nTups();
+    }
+
+    private int nTups() {
+        return this.bucketList.stream().reduce(0, Integer::sum);
+    }
+
     /**
      * @return
      *     the average selectivity of this histogram.
@@ -69,6 +187,11 @@ public class IntHistogram {
      */
     public String toString() {
         // some code goes here
-        return null;
+        StringBuffer sb = new StringBuffer();
+        sb.append(String.format("min: %s \t max: %s \t width: %d\n",
+            min, max, width));
+        bucketList.forEach(s -> sb.append(s).append("\t"));
+        sb.append("\n");
+        return sb.toString();
     }
 }
